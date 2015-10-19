7c9942a6dbde7cb39437e72f5fa7bf4345d513d7
Satoki Ogiso <ogiso@aclab.esys.tsukuba.ac.jp>
2015-10-19 20:15:57 +0900

    [modify] insatall ros indigo

--- Vagrantfile
+++ Vagrantfile
@@ -67,7 +67,7 @@ Vagrant.configure(2) do |config|
   # config.push.define "atlas" do |push|
   #   push.app = "YOUR_ATLAS_USERNAME/YOUR_APPLICATION_NAME"
   # end
-  config.vm.provision :shell, path: "bootstrap.sh"
+  config.vm.provision :shell, path: "bootstrap.sh", privileged: false
   # Enable provisioning with a shell script. Additional provisioners such as
   # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
   # documentation for more information about their specific syntax and use.
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
-sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-apt-key adv --keyserver hkp://pool.sks-keyservers.net:80 --recv-key 0xB01FA116
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net:80 --recv-key 0xB01FA116
 
-apt-get update
+sudo apt-get update
+
+sudo apt-get install ros-indigo-desktop-full -y --force-yes
+
+sudo rosdep init
+rosdep update
+
+echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+
+sudo apt-get install python-rosinstall -y --force-yes
 
