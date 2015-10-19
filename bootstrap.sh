47e2b180b5bd580651ae7d0944d5de195efcfb72
Satoki Ogiso <ogiso@aclab.esys.tsukuba.ac.jp>
2015-10-19 17:47:35 +0900

    [add] add vagrantfile

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+apt-key adv --keyserver hkp://pool.sks-keyservers.net:80 --recv-key 0xB01FA116
+
+apt-get update
+
